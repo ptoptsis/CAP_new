3c673769adfaa21f463e1b3255abce2ac390e8cf
ptoptsis <ptoptsis@gmail.com>
2020-02-03 13:22:53 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -14,7 +14,7 @@
 
 // Based on the template node_modules/cordova-android/bin/templates/project/Activity.java
 
-package com.moodle.moodlemobile;
+package cy.com.cap.academy2;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
